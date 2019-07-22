bca2efa9188705d9baa18b0c4c0e86732f507ca7
Chiayi Lin <angel05446a@gmail.com>
2019-07-22 09:42:11 +0800

    separate two viewModel

--- MVC_Q2/MVC_Q2/ViewModels/ProductDetailViewModel.cs
+++ MVC_Q2/MVC_Q2/ViewModels/ProductDetailViewModel.cs
@@ -1,22 +1,17 @@
-﻿using PagedList;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace MVC_Q2.ViewModels
 {
-    public class ProductViewModel
-    {
-        public IPagedList<Product> productList { get; set; }
-    }
-
-    public class Product
+    public class ProductDetailViewModel
     {
         public int Id { get; set; }
         public string Locale { get; set; }
         public string Product_Name { get; set; }
+        public string Price { get; set; }
+        public string Promote_Price { get; set; }
         public string Create_Date { get; set; }
     }
-
 }
